24e7cb3053915ac181aca9526a41fbe8002fbcac
Max Carmona <megarc47@asu.edu>
2024-07-11 22:24:59 -0400

    removing slash

--- api/listarPrecios.js
+++ api/listarPrecios.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 export function GET() {
-  let usersPath = path.join(process.cwd(), '/api/content/get-listar-precios.html');
+  let usersPath = path.join(process.cwd(), 'api/content/get-listar-precios.html');
   let file = fs.readFileSync(usersPath);
   return new Response(file);
 }
