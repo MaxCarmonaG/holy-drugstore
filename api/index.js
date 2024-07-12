d831b73162bf641e03377008432ec53d9643521e
Max Carmona <megarc47@asu.edu>
2024-07-11 23:00:44 -0400

    new approach

--- api/index.js
+++ api/index.js
@@ -1,13 +1,19 @@
 import express from 'express';
 import path from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
+const app = express();
+
+/* const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(process.cwd(), 'public'))); */
 
 /* app.get('/listarPrecios', (req, res) => {
   res.sendFile(path.resolve('api/content/get-listar-precios.html'));
 }); */
+
+app.get('/', (req, res) => res.send('Express on Vercel'));
+
+app.listen(3000, () => console.log('Server ready on port 3000.'));
 
 export default app;
