40414d0fc3202de6bbbd614527b2bacff162d5da
Max Carmona <megarc47@asu.edu>
2024-07-11 22:47:24 -0400

    new try

--- api/index.js
+++ api/index.js
@@ -1,3 +1,10 @@
-import app from '../app';
+const express = require('express');
+const app = express();
 
-export default app;
+app.use(express.static(path.join(__dirname, 'public')));
+
+/* app.get('/listarPrecios', (req, res) => {
+  res.sendFile(path.resolve('api/content/get-listar-precios.html'));
+}); */
+
+module.exports = app;
